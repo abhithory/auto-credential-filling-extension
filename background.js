88a9170a4b42938a42f623a1e8e2b815661355f9
theteamisbest <abhithory@gmail.com>
2023-05-15 16:17:19 +0530

    added logic for saving password in next tab after login

--- background.js
+++ background.js
@@ -1,59 +1,20 @@
 console.log("background.js loaded");
 
-chrome.webNavigation.onCompleted.addListener(({ tabId, frameId }) => {
-    if (frameId !== 0) return;
-    chrome.scripting.executeScript({
-        target: { tabId },
-        function: fillingPassword
-    })
-})
-
-const fillingPassword = async () => {
-
-    try {
-
-        //  use chrome .includes here both sides
-        console.log("website url", location.href);
-        const emailToFill = "thisisemail@gmail.com"
-        const passToFill = "this is password"
-
-        let inputs = Array.from(document.getElementsByTagName("input"));
-        const inputsOnlyPass = document.querySelectorAll('input[type="password"]')[0];
-        const relatedForm = inputsOnlyPass.closest('form');
-
-        console.log(relatedForm);
+// chrome.webNavigation.onCompleted.addListener(({ tabId, frameId }) => {
+//     if (frameId !== 0) return;
 
 
-        let inputsOnlyEmail;
-        const indexOfPassField = inputs.indexOf(inputsOnlyPass);
-        if (indexOfPassField > 0) {
-            inputsOnlyEmail = inputs[indexOfPassField - 1];
-            inputsOnlyEmail.value = emailToFill;
-        }
-        savingPassword(relatedForm, inputsOnlyEmail.name, inputsOnlyPass.name)
 
-        inputsOnlyPass.value = passToFill;
-    } catch (error) {
-        console.log("error", error);
-    }
-
-    function savingPassword(_form, _emailId, _passId) {
-
-        console.log(_emailId);
-        _form.addEventListener("submit", function (e) {
-            // e.preventDefault()
-            try {
-                console.log(_form.elements[_emailId].value);
-                console.log(_form.elements[_passId].value);
+//     chrome.tabs.sendMessage(tabId, {
+//         "isLoaded": true
+//     });
 
 
-                alert("go")
-                // chrome.extension.sendRequest({ 'name': 'form_submit', 'data': data }, function () { /* callback from request */ }, false);
-            } catch (error) {
-                console.log("error", error);
-            }
-        });
-    }
-}
 
+//     // chrome.scripting.executeScript({
+//     //     target: { tabId },
+//     //     function: fillingPassword,
+//     //     args: [tabId]
+//     // })
+// })
 
